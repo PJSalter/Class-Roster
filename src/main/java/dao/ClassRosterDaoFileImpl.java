e004434bbb0bbd14a819c2bd44b029d6c0369704
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-07 19:26:56 +0100

    return a list of all Student objects stored in the map

--- src/main/java/dao/ClassRosterDaoFileImpl.java
+++ src/main/java/dao/ClassRosterDaoFileImpl.java
@@ -2,6 +2,7 @@ package dao;
 
 import dto.Student;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,7 +19,7 @@ public class ClassRosterDaoFileImpl implements classRosterDao {
 
     @Override
     public List<Student> getAllStudents() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return new ArrayList<Student>(students.values());
     }
 
     @Override
