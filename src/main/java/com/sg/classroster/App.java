9293622f3b752395abf3df5f5c3e732fab4deb8d
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-08 00:17:43 +0100

    updated main in App after refactoring view and controller

--- src/main/java/com/sg/classroster/App.java
+++ src/main/java/com/sg/classroster/App.java
@@ -1,11 +1,18 @@
 package com.sg.classroster;
 
 import controller.ClassRosterController;
+import dao.ClassRosterDaoFileImpl;
+import dao.classRosterDao;
+import ui.ClassRosterView;
+import ui.UserIO;
+import ui.UserIOConsoleImpl;
 
 public class App {
     public static void main(String[] args) {
-
-        ClassRosterController controller = new ClassRosterController();
+        UserIO myIo = new UserIOConsoleImpl();
+        ClassRosterView myView = new ClassRosterView(myIo);
+        classRosterDao myDao = new ClassRosterDaoFileImpl();
+        ClassRosterController controller = new ClassRosterController(myDao, myView);
         controller.run();
     }
 }
